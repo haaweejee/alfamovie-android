f1537f464610553aea42763c13cb841642dae90d
Hendrawan Wibowo Jatmiko <haaweejee.projects@gmail.com>
2025-01-21 22:25:39 +0700

    enhance(common): Enhance ViewState.ViewError
    Explanation:
    - Create new val title to give Error title
    - Add Default Title and Message for condition

--- app/src/main/java/id/haaweejee/test/alfagift/alfamovie/presentation/common/ViewState.kt
+++ app/src/main/java/id/haaweejee/test/alfagift/alfamovie/presentation/common/ViewState.kt
@@ -6,14 +6,25 @@ sealed class ViewState<out T> {
     data object ViewEmpty : ViewState<Nothing>()
     data object ViewLoading : ViewState<Nothing>()
     data class ViewSuccess<out T>(val data: T) : ViewState<T>()
-    data class ViewError(val message: String) : ViewState<Nothing>()
+    data class ViewError(val title: String, val message: String) : ViewState<Nothing>()
     data class ViewFailed(val exception: Exception) : ViewState<Nothing>()
 }
 
 fun <T> NetworkResult<T>.mapToViewState(): ViewState<T> {
     return when (this) {
         is NetworkResult.ResultSuccess -> ViewState.ViewSuccess(this.data)
-        is NetworkResult.ResultError -> ViewState.ViewError("Network error: $message (Code: $code)")
+        is NetworkResult.ResultError -> ViewState.ViewError(
+            title = when (code) {
+                401 -> "Unauthorized"
+                404 -> "Page Not Found"
+                else -> "Something went wrong"
+            },
+            message = when (code) {
+                401 -> "You Don't Have Permission to access this Page"
+                404 -> "Page Not Found, Please try again."
+                else -> "Something went wrong, Please try again."
+            }
+        )
         is NetworkResult.ResultFailed -> ViewState.ViewFailed(this.exception)
     }
 }
