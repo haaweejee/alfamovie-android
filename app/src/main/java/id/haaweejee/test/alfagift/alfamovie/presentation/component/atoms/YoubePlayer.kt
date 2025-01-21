9603ce5680c1f381d1d3e3a0b66aca312389fd92
Hendrawan Wibowo Jatmiko <haaweejee.projects@gmail.com>
2025-01-21 23:34:38 +0700

    enhance(presentation): Add Log in YoutubePlayer

--- app/src/main/java/id/haaweejee/test/alfagift/alfamovie/presentation/component/atoms/YoubePlayer.kt
+++ app/src/main/java/id/haaweejee/test/alfagift/alfamovie/presentation/component/atoms/YoubePlayer.kt
@@ -1,8 +1,10 @@
 package id.haaweejee.test.alfagift.alfamovie.presentation.component.atoms
 
+import android.util.Log
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.viewinterop.AndroidView
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.PlayerConstants
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView
@@ -17,6 +19,14 @@ fun YoutubePlayer(key: String, modifier: Modifier = Modifier) {
                     override fun onReady(youTubePlayer: YouTubePlayer) {
                         super.onReady(youTubePlayer)
                         youTubePlayer.loadVideo(key, 0f)
+                    }
+
+                    override fun onError(
+                        youTubePlayer: YouTubePlayer,
+                        error: PlayerConstants.PlayerError
+                    ) {
+                        super.onError(youTubePlayer, error)
+                        Log.d("YoutubePlayer", "Error: $error")
                     }
                 },
             )
