49884d801c4ad62ad63d81fe627e82751f2838b2
Hendrawan Wibowo Jatmiko <haaweejee.projects@gmail.com>
2025-01-22 19:31:21 +0700

    enhance(domain): Update GetMovieVideoUseCaseImpl
    Explanation:
    - Update Logic to use Filter, with Filter type is "Trailer" and site is "YouTube" and take last value of the filter list

--- app/src/main/java/id/haaweejee/test/alfagift/alfamovie/domain/usecase/GetMovieVideoUseCaseImpl.kt
+++ app/src/main/java/id/haaweejee/test/alfagift/alfamovie/domain/usecase/GetMovieVideoUseCaseImpl.kt
@@ -4,24 +4,20 @@ import id.haaweejee.test.alfagift.alfamovie.data.common.NetworkResult
 import id.haaweejee.test.alfagift.alfamovie.domain.entities.MovieVideo
 import id.haaweejee.test.alfagift.alfamovie.domain.repository.MovieRepository
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.firstOrNull
 import kotlinx.coroutines.flow.flow
 
 class GetMovieVideoUseCaseImpl(private val repository: MovieRepository) : GetMovieVideoUseCase {
     override fun invoke(id: Int): Flow<NetworkResult<MovieVideo>> = flow {
-        val result = repository.fetchVideos(id).first()
+        val result = repository.fetchVideos(id).firstOrNull()
 
         if (result !is NetworkResult.ResultSuccess) {
             emit(NetworkResult.ResultError(404, "Data not found"))
             return@flow
         } else {
             val data = result.data
-            val trailer = data.find { it.type == "Trailer" || it.type == "Teaser" } ?: MovieVideo(
-                key = "",
-                site = "",
-                type = ""
-            )
-            emit(NetworkResult.ResultSuccess(trailer))
+            val trailer = data.filter { it.type == "Trailer" && it.site == "YouTube" }
+            emit(NetworkResult.ResultSuccess(trailer.last()))
             return@flow
         }
     }
